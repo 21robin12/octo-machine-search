fff7cecf3c6b1bc9e5a47399531c0f99b211fb0d
robin.herd <robin.herd@riskfirst.com>
2017-08-17 12:17:07 +0100

    make request

--- OctoMachineSearch/Program.cs
+++ OctoMachineSearch/Program.cs
@@ -1,14 +1,26 @@
 ﻿using OctoMachineSearch.Config;
 using System;
+using System.IO;
+using System.Net.Http;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace OctoMachineSearch
 {
     class Program
     {
+        private static readonly HttpClient client = new HttpClient();
+
         static void Main(string[] args)
         {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
             var config = ConfigManager.Config;
+            var url = $"{config.OctoUrlBase}/api/machines/all?apikey={config.ApiKey}";
+            var responseString = await client.GetStringAsync(url);
             Console.WriteLine("Hello World!");
         }
     }

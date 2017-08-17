160b1e446c293323bd58e72dd45f0d46c9bedfd6
robin.herd <robin.herd@riskfirst.com>
2017-08-17 12:32:02 +0100

    implement search

--- OctoMachineSearch/Program.cs
+++ OctoMachineSearch/Program.cs
@@ -1,6 +1,9 @@
-﻿using OctoMachineSearch.Config;
+﻿using Newtonsoft.Json;
+using OctoMachineSearch.Config;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -13,15 +16,42 @@ namespace OctoMachineSearch
 
         static void Main(string[] args)
         {
-            RunAsync().Wait();
+            RunAsync(args[0]).Wait();
         }
 
-        static async Task RunAsync()
+        static async Task RunAsync(string searchText)
         {
             var config = ConfigManager.Config;
             var url = $"{config.OctoUrlBase}/api/machines/all?apikey={config.ApiKey}";
             var responseString = await client.GetStringAsync(url);
-            Console.WriteLine("Hello World!");
+            var result = JsonConvert.DeserializeObject<IList<Machine>>(responseString);
+            var matching = result.Where(x => x.Roles.Any(r => r.ToLower().Contains(searchText))).ToList();
+
+            if (matching.Count == 0)
+            {
+                Console.WriteLine("No results");
+            }
+            else
+            {
+                PrintResults(matching.ToList());
+            }            
         }
+
+        static void PrintResults(IList<Machine> results)
+        {
+            Console.WriteLine();
+            var longestName = results.OrderByDescending(x => x.Name.Length).FirstOrDefault().Name.Length;
+            foreach(var result in results)
+            {
+                Console.WriteLine($"{result.Name.PadRight(longestName + 5)}{string.Join(" | ", result.Roles)}");
+            }
+        }
+    }
+
+    class Machine
+    {
+        public string Name { get; set; }
+        public IList<string> EnvironmentIds { get; set; }
+        public IList<string> Roles { get; set; }
     }
 }
